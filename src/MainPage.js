6ffff587057b00a31537c7875c1bb9f93f53a6da
hactm <mikitastrachynski@gmail.com>
2024-05-18 22:47:19 +0300

    new commit

--- src/MainPage.js
+++ src/MainPage.js
@@ -40,7 +40,7 @@ const HomePage = () => {
     return (
         <div>
             <Card className="bg-dark text-black">
-                <Card.Img src="http://164.92.207.100/main-image.avif" alt="Card image" />
+                <Card.Img src="http://164.92.207.100/modelsicon/MainIcon.png" alt="Card image" />
                 <Card.ImgOverlay>
                     <Navbar collapseOnSelect expand="lg" className="shadow-lg">
                         <Container>
@@ -99,6 +99,25 @@ const HomePage = () => {
                     </CSSTransition>
                 </Card.ImgOverlay>
             </Card>
+            <Navbar collapseOnSelect expand="lg" className="shadow-lg"   style={{display: 'flex', flexDirection: 'column-reverse', height: '100%'}} classNames='parent'>
+                <Container>
+                    <Navbar.Brand href="/">
+                            Mercedes-Benz
+                            <img width="50" src="http://164.92.207.100/mercedes.png" />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto"> {/* Space between elements */}
+                            </Nav>
+                            <Nav>
+                                <Nav.Link onClick={() => setShowCatalog(!showCatalog)}>
+                                    <span className="navbar-toggler-icon" style={{marginRight: '12px'}}></span>
+                                   Каталог
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 }
