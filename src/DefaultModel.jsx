ef15833300dda29da3c0e98b961e481e0d7cf481
hactm <mikitastrachynski@gmail.com>
2024-05-22 04:15:49 +0300

    config

--- src/DefaultModel.jsx
+++ src/DefaultModel.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Card from 'react-bootstrap/Card';
-import {Button, CardImg, CardImgOverlay, CardTitle} from "react-bootstrap";
+import {Button, CardImg, CardImgOverlay, CardTitle, Col, Row} from "react-bootstrap";
 import { CSSTransition } from 'react-transition-group'
 import './index.css'
 import { useParams } from 'react-router-dom'
@@ -20,18 +20,33 @@ import './App.css';
 const DefaultModel = () => {
     let {modelId}=useParams()
     const [defaultModel, setDefaultModel] = useState({});
+    const [imageUrl, setImageUrl] = useState("http://164.92.207.100/modelsicon/conf_a_class.png");
+    const [configElement, setConfigElement] = useState('wheels');
 
     const getApiData = async () => {
         const response = await fetch(
             `http://164.92.207.100/default_models/${modelId}`
         ).then((response) => response.json());
-        console.log(`http://164.92.207.100/default_models/${modelId}`)
-        console.log(response)
+        if (!window.sessionStorage.getItem(`wheel_id_${modelId}`)) {
+            window.sessionStorage.setItem(`wheel_id_${modelId}`, response.wheels[0].id);
+        }
+        if (!window.sessionStorage.getItem(`color_id_${modelId}`)) {
+            window.sessionStorage.setItem(`color_id_${modelId}`, response.colors[0].id);
+        }
+
         setDefaultModel(response);
+    };
+
+    const getModelData = async () => {
+        const response = await fetch(
+            `http://164.92.207.100/default_models/${modelId}?wheel_id=${window.sessionStorage.getItem(`wheel_id_${modelId}`)}&color_id=${window.sessionStorage.getItem(`color_id_${modelId}`)}`
+        ).then((response) => response.json());
+        setImageUrl(response.image_url)
     };
 
     useEffect(() => {
         getApiData();
+        getModelData();
     }, []);
 
 
@@ -59,61 +74,60 @@ const DefaultModel = () => {
                 </Card.ImgOverlay>
                 
             </Card>
+
             <Card>
                 <Card.Img src='http://164.92.207.100/modelsicon/conf_a_class.png' alt="Card image"/>
-                <Container >
-                    <CardImgOverlay  style={{backgroundColor: 'rgba(52, 52, 52, 0.8)',height: '64%', width: '90%', margin:'5%', display: 'flex', flexDirection: 'row-reverse',display:'flex', alignContent:"center" }}classNames="parent">
-                        <Container style={{
-                            display: 'flex', width: '150px', height: '150px',
-                            justifyContent: 'center',
-                            alignContent: 'center' }}
-                            classNames="parent">
+                <CardImgOverlay >
+                    <Row style={{backgroundColor: 'rgba(52, 52, 52, 0.8)',  width: '90%', margin:'3%', padding:'1%', alignContent:"center" }}>
+                        <Col>
+                            <Row>
+                                <Container style={{marginBottom:'3%', height: '700px',width: '880px'}}>
+                                    <CardImg src={imageUrl}/>
+                                </Container>
+                            </Row>
+                            <Row>
+                                <Container>
+                                    <Container style={{display: 'flex', flexDirection:'row'}}>
+                                        {configElement === 'wheels' && defaultModel?.wheels?.map((wheel) =>
+                                            <>
+                                                <Button onClick={() => {
+                                                    window.sessionStorage.setItem(`wheel_id_${modelId}`, wheel.id);
+                                                    getModelData();
+                                                }} padding="0" style={{backgroundColor: 'rgba(52, 52, 52, 0.2)',width: '180px', height: '170px',}}>
+                                                    <Card.Img src={wheel.image_url} style={{margin: '0', width: '150px', height: '150px'}}/>
+                                                </Button>
+                                            </>
+                                        )}
 
-                            <Container style={{display: 'fles', flexDirection:'column', justifyContent:'space-around'}}>
-                                <Button variant="primary" padding="0" style={{backgroundColor: 'rgba(52, 52, 52, 0.2)',width: '180px', height: '170px',}}>    
-                                    <Card.Img src='http://164.92.207.100/modelsicon/image_tapki.png' style={{margin: '0', width: '150px', height: '150px',
-                                        display: 'flex',
-                                        justifyContent: 'flex-end' }}
-                                        classNames="parent"/>
-                                </Button>        
-                                <Button variant="primary" padding="0" style={{backgroundColor: 'rgba(52, 52, 52, 0.2)',width: '180px', height: '150px',}}>
-                                    <Card.Img src='http://164.92.207.100/modelsicon/conf_korpus.png' style={{ width: '155px', height: '124px',
-                                        display: 'flex',
-                                        justifyContent: 'flex-end',
-                                        }}
-                                        classNames="parent"/>
-                                </Button>
-                                <Button variant="primary" padding="0" style={{backgroundColor: 'rgba(52, 52, 52, 0.2)',width: '180px', height: '150px',}}>
-                                    <Card.Img src='http://164.92.207.100/modelsicon/amg_paket_conf.png' style={{ width: '155px', height: '110px',
-                                        display: 'flex',
-                                        justifyContent: 'flex-end',
-                                        }}
-                                        classNames="parent"/>
-                                </Button>
-                            </Container>    
-                            <Container style={{
-                                display: 'flex', width: '150px',
-                                justifyContent: 'center',
-                                alignContent: 'center' }}
-                                classNames="parent">
-                            </Container>    
-                            
-                        </Container> 
-                        <Container
-                            style={{height: '70%', width: '70%',margin: '0',
-                            display: 'flex',
-                            justifyContent:'flex-start',
-                            alignItems:'flex-start'}}
-                            classNames="parent">
-                                <CardImg src='http://164.92.207.100/modelsicon/conf_a_class.png'/>
-                        </Container>
-                        <Container style={{width: '130px', height:'130px', padding:"0",
-                         display: 'flex',
-                          alignItems:'flex-end'}}classNames="parent"></Container>
-                          
-                    </CardImgOverlay>
-                </Container>
-            </Card>      
+                                        {configElement === 'colors' && defaultModel?.colors?.map((color) =>
+                                            <>
+                                                <Button onClick={() => {
+                                                    window.sessionStorage.setItem(`color_id_${modelId}`, color.id);
+                                                    getModelData();
+                                                }} padding="0" style={{backgroundColor: 'rgba(52, 52, 52, 0.2)',width: '180px', height: '170px',}}>
+                                                    <Card.Img src={color.image_url} style={{margin: '0', width: '150px', height: '150px'}}/>
+                                                </Button>
+                                            </>
+                                        )}
+                                    </Container>
+                                </Container>
+                            </Row>
+                        </Col>
+                        <Col lg="3">
+                            <Container>
+                                <Container style={{display: 'flex', flexDirection:'column'}}>
+                                    <Button onClick={() => setConfigElement('wheels')} variant="primary" padding="0" style={{backgroundColor: 'rgba(52, 52, 52, 0.2)',width: '180px', height: '170px',}}>
+                                        <Card.Img src='http://164.92.207.100/modelsicon/image_tapki.png' style={{width: '150px', height: '150px'}}/>
+                                    </Button>
+                                    <Button onClick={() => setConfigElement('colors')} variant="primary" padding="0" style={{backgroundColor: 'rgba(52, 52, 52, 0.2)',width: '180px', height: '150px',}}>
+                                        <Card.Img src='http://164.92.207.100/modelsicon/conf_korpus.png' style={{ width: '155px', height: '124px'}}/>
+                                    </Button>
+                                </Container>
+                            </Container>
+                        </Col>
+                    </Row>
+                </CardImgOverlay>
+            </Card>
             <Footer></Footer>
         </div>
         );}
